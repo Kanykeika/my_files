466d5163c52f2b55a6e7e4a6062544c927f9a517
Kanykei <kanykei.kalykova@iaau.edu.kg>
2015-02-19 08:24:36 +0600

    Create hello.rb

---
+++ hello.rb
@@ -0,0 +1,24 @@
+def greet(name)
+=begin
+	(string) -> string
+	
+	this method has to accept string
+	and return string
+	>  greet("Azamat")
+	=> "Hello, Azamat!"
+=end
+	"Hello, #{name}!"
+end
+
+
+def hello
+=begin
+	() -> string
+	this method does not have to accept
+	any arguments and it has to return
+	string
+	>  hello()
+	=> "Hello!"
+=end
+ "Hello!"
+end
